0baf682163e18441d7e2a78d438662ab87cd3540
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-02-13 21:49:00 -0600

    Convirtiendo función a anónima en evento input

--- js/validar-texto.js
+++ js/validar-texto.js
@@ -2,31 +2,7 @@ var campoAgregar = document.querySelector("#nueva-palabra");
 var campoLetraIngresada = document.querySelector("#letra-ingresada");
 var filtro = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
 
-campoAgregar.addEventListener("input", validarCampoAgregar);
-
-campoLetraIngresada.addEventListener("input", function () {
-
-    var textoMayusculas = "";
-    var textoValidado = "";
-    var valorCampo = this.value;
-
-    for (var j = 0; j < filtro.length; j++) {
-        if (filtro[j] == valorCampo) {
-            textoValidado += valorCampo;
-        }
-    }
-
-    textoMayusculas = textoValidado.toUpperCase();
-    this.value = textoMayusculas;
-
-
-    if (textoMayusculas != "") {
-        dibujarLetra(textoMayusculas);
-    }
-});
-
-function validarCampoAgregar() {
-
+campoAgregar.addEventListener("input", function () {
     var textoMayusculas = "";
     var textoValidado = "";
     var valorCampo = campoAgregar.value;
@@ -41,8 +17,26 @@ function validarCampoAgregar() {
 
     textoMayusculas = textoValidado.toUpperCase();
     campoAgregar.value = textoMayusculas;
-}
+});
 
+campoLetraIngresada.addEventListener("input", function () {
+    var textoMayusculas = "";
+    var textoValidado = "";
+    var valorCampo = this.value;
+
+    for (var j = 0; j < filtro.length; j++) {
+        if (filtro[j] == valorCampo) {
+            textoValidado += valorCampo;
+        }
+    }
+
+    textoMayusculas = textoValidado.toUpperCase();
+    this.value = textoMayusculas;
+
+    if (textoMayusculas != "") {
+        dibujarLetra(textoMayusculas);
+    }
+});
 
 function posicionarPuntero(campo) {
     return campo.focus();
