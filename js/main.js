f769efc70f3db69c8d566db16bfd1b0bafd41472
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-02-14 14:52:01 -0600

    Agregando parámetro de la variable global array

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function generarJuego() {
 
-    palabraAleatoria = generarPalabraAleatoria();
+    palabraAleatoria = generarPalabraAleatoria(array);
     letrasAcertadas = [];
     letrasFallidas = [];
     pLetrasFallidas.textContent = "";
