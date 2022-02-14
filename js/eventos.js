8e293eddc0c06f25d66e761a64d64b88d9390ceb
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-02-13 20:34:58 -0600

    Probando limitar caracteres en eventos

--- js/eventos.js
+++ js/eventos.js
@@ -35,7 +35,7 @@ letraUsuario.addEventListener("input", function () {
     var letraEntrada;
     letraEntrada = validarCampoLetraIngresada();
 
-    if (letraEntrada != "" && letraEntrada.length < 1) {
+    if (letraEntrada != "") {
         dibujarLetra(letraEntrada);
     }
 });
